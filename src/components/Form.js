1d820db60eec975c15a40d6476189f74e0e576a2
Max Sciarra <maxsciarra@Maxs-MacBook-Pro-SSD.local>
2020-07-17 19:11:24 -0700

    form built

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import './Form.css'
 
 class Form extends Component {
+    
+    submitHandler(){
+        console.log('test');
+    }
+    
     render() {
         return (
             <div>
-                
+                <form>
+                    <h1>Hello, welcome to our platform.</h1>
+                    <h2>Please proceed with sign up</h2>
+                    <p>First Name</p>
+                    <input type='text' />
+                    <p>Last Name</p>
+                    <input type='text' />
+                    <p>Date of Birth</p>
+                    <input type='text' />
+                    <p>Username</p>
+                    <input type='text' />
+                    <p>Password</p>
+                    <input type='text' />
+                    <p ></p>
+                    <button onClick={this.submitHandler}>Submit</button>
+                    
+                </form>
+                {/* <button onClick={this.submitHandler}>Submit</button> */}
             </div>
         )
     }
