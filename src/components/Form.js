fe3b34c20e4441affded64c31467071bf1e4dfb4
Max Sciarra <maxsciarra@Maxs-MacBook-Pro-SSD.local>
2020-07-18 19:54:01 -0700

    formatted form ui

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import './Form.css'
+import './Form.css'
 
 
 class Form extends Component {
@@ -58,31 +58,36 @@ class Form extends Component {
     render() {
         return (
             <div>
-                <h1>Hello, welcome to our platform.</h1>
-                <h2>Please proceed with sign up</h2>
-                <form className = 'form' onSubmit = {this.submitHandler}>
-                    <div>
-                        <label className = 'label'>First Name</label>
-                        <input type='text' value={this.state.firstName} onChange = {this.firstNameChangeHandler}/>
-                    </div>
-                    <div>
-                        <label className = 'label'>Last Name</label>
-                        <input type='text' value={this.state.lastName} onChange = {this.lastNameChangeHandler}/>
-                    </div>
-                    <div>
-                        <label className = 'label'>Date of Birth</label>
-                        <input type='text' value={this.state.dateOfBirth} onChange = {this.dateOfBirthChangeHandler}/>
-                    </div>
-                    <div>
-                        <label className = 'label'>Username</label>
-                        <input type='text' value={this.state.username} onChange = {this.usernameChangeHandler}/>
-                    </div>
-                    <div>
-                        <label className = 'label'>Password</label>
-                        <input type='text' value={this.state.password} onChange = {this.passwordChangeHandler}/>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
+                <div>
+                    <h1>Hello, welcome to our platform.</h1>
+                    <h2>Please proceed with sign up</h2>
+                    <form className = 'form' onSubmit = {this.submitHandler}>
+                        <div>
+                            <label>First Name</label>
+                            <input type='text' value={this.state.firstName} onChange = {this.firstNameChangeHandler}/>
+                        </div>
+                        <div>
+                            <label>Last Name</label>
+                            <input type='text' value={this.state.lastName} onChange = {this.lastNameChangeHandler}/>
+                        </div>
+                        <div>
+                            <label>Date of Birth</label>
+                            <input type='text' value={this.state.dateOfBirth} onChange = {this.dateOfBirthChangeHandler}/>
+                        </div>
+                        <div>
+                            <label>Username</label>
+                            <input type='text' value={this.state.username} onChange = {this.usernameChangeHandler}/>
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type='text' value={this.state.password} onChange = {this.passwordChangeHandler}/>
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+                <div>
+                    <p className='dobexample'>example: 6/31/2020</p>
+                </div>
             </div>
         )
     }
